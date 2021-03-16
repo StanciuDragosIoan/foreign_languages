92df75024bf0329501c8dbb991eb9db68636ecf3
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-03-16 15:28:35 +0200

    test

--- japanese/assets/js/script.js
+++ japanese/assets/js/script.js
@@ -1,35 +1,32 @@
- 
-
 const toggleTranslation = (e) => {
-    const translation = e.target.previousSibling.previousSibling; 
-   
-    if(translation.className === 'hide'){
-        translation.className = 'kana text-center';
-        e.target.innerText = 'Hide';
-    } else {
-        translation.className = 'hide';
-        e.target.innerText = 'Show';
-    }
-}
+  const translation = e.target.previousSibling.previousSibling;
 
-const check  = (e, answer) => {
-    const label = e.target.previousSibling.previousSibling;
-    console.log(label)
-    const input =   e.target;
-    console.log(input)
+  if (translation.className === "hide") {
+    translation.className = "kana text-center";
+    e.target.innerText = "Hide";
+    e.target.style.backgroundColor = "rgb(189, 14, 81)";
+  } else {
+    translation.className = "hide";
+    e.target.innerText = "Show";
+    e.target.style.backgroundColor = "blue";
+  }
+};
 
- if(e.target.value !== answer){
-       console.log('wrong');
-   
-       label.style.border = "5px solid red";
-       input.style.border = "5px solid red";
-        label.innerText = "Wrong! X_x"
-    } else {
-        label.style.border = "5px solid green";
-        input.style.border = "5px solid green";
-        label.innerText = "Correct! ^_^"
-    }
+const check = (e, answer) => {
+  const label = e.target.previousSibling.previousSibling;
+  console.log(label);
+  const input = e.target;
+  console.log(input);
 
- 
-    
-}
+  if (e.target.value !== answer) {
+    console.log("wrong");
+
+    label.style.border = "5px solid red";
+    input.style.border = "5px solid red";
+    label.innerText = "Wrong! X_x";
+  } else {
+    label.style.border = "5px solid green";
+    input.style.border = "5px solid green";
+    label.innerText = "Correct! ^_^";
+  }
+};
